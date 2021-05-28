32ff5f14263c698a2c07f6edbc5150614417a9e4
Gwan-Gyu Lee <fred.lee.9808@icloud.com>
2021-05-28 11:33:38 +0900

    Update AppDelegate.swift

--- OSC_Pencil/OSC_Pencil/AppDelegate.swift
+++ OSC_Pencil/OSC_Pencil/AppDelegate.swift
@@ -4,6 +4,9 @@
 //
 //  Created by Gwan-Gyu Lee(frd.lee@icloud.com) on 2021/02/11.
 //
+//  This application contains copyrighted software under MIT License.     
+//  SwiftOSC - Copyright (c) 2017 Devin Roth    
+//  Colorful - Copyright (c) 2011 Ryota Hayashi
 
 import UIKit
 
